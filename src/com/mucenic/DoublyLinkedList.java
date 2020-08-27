4768629ef2ebab36f9fbb62ff4c7906a99bf7b33
Mucenicalexandru <mucenic.alexandru@gmail.com>
2020-08-27 12:19:05 +0300

    Doubly Linked List Java

---
+++ src/com/mucenic/DoublyLinkedList.java
@@ -0,0 +1,9 @@
+package com.mucenic;
+
+public class DoublyLinkedList {
+    //Each node points to the previous and to the next node
+    //The list has a head and a tail
+
+    //head points to next and previous
+    //tail is the last item, points to null and previous
+}
